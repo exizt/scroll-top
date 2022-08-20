fee8e6247fa8dde5b3e413e8bbb55c4d115ce88d
exizt <e2xist@gmail.com>
2022-08-20 18:43:07 +0900

    minor

--- devnote.md
+++ devnote.md
@@ -5,26 +5,15 @@
 
 
 # 빌드
-esnext 버전
-* `npm run build` : tsc + minify
-  * `npm run tsc` : ts -> js
-  * `npm run minify` : js -> min.js
-
-
-es5 버전 (support ie11)
-* `npm run build.legacy` : tsc (es5.js) + minify (es5.min.js)
-  * `npm run tsc.legacy` : ts -> es5.js
-  * `npm run minify.legacy` : es5.js -> es5.min.js
-
-
-css 컴파일
-* `npm run build.css` : cs -> min.css
+* `npm run build` : js 생성 + legacy.js 생성
+    * `npm run webpack` : ts -> js (with minify)
+    * `npm run webpack.legacy` : ts -> legacy.js (with minify)
 
 
 # 버저닝
 1. major : 대대적 변경.
 2. minor : ts 코드에 사소한 변경.
-3. build : 의존성 변경, 환경 변경으로 인한 업데이트. 또는 빌드.
+3. build : (누적 카운팅) 의존성 변경, 환경 변경으로 인한 업데이트. 또는 빌드.
 
 
 
@@ -33,40 +22,29 @@ css 컴파일
 2. ts 코드 상단에서 버전 변경
 
 
-# 특이사항
-
-중요한 것은 css 로 화살표 모양을 만들었다는 점이다. 그 외에는 그냥 동작을 위한 스크립트. 
-
-하다보니 css 로 구현했는데, 기회가 되는대로 svg로 하면 더 간단해질 것도 같음... 
-
-
-
-# TO-Do
-
-1. legacy.min.js를 호출하면 ie등 예전 브라우저가 되게하고, 기본 min.js를 호출하면, 예전 브라우저에서는 아예 버그없이 동작되지 않도록 하기.
-
 
 # 프로젝트 셋팅
-1. `npm install` : `node_modules` 생성됨.
+1. `npm install` : `node_modules` 생성 및 타입스크립트 환경 구성됨.
 
 
-## 프로젝트 구성 과정에서의 히스토리
-1. npm 셋팅
-    ```shell
-    npm install --save-dev typescript
-    npm install --save-dev webpack webpack-cli ts-loader
-    npm install --save-dev style-loader css-loader
-    ```
-    - `typescript` : 타입스크립트 기능
-    - `webpack`, `webpack-cli` : 웹팩 및 웹팩 cli
-    - `ts-loader` : 웹팩에서 typescript를 로드하는 웹팩 로더
-    - `style-loader`, `css-loader` : css파일을 병합하기 위한 웹팩 로더
+# 프로젝트 구성 과정에서의 히스토리
+## npm 셋팅
+```console
+$ npm install --save-dev typescript
+$ npm install --save-dev webpack webpack-cli ts-loader
+$ npm install --save-dev style-loader css-loader
+```
+- `typescript` : 타입스크립트 기능
+- `webpack`, `webpack-cli` : 웹팩 및 웹팩 cli
+- `ts-loader` : 웹팩에서 typescript를 로드하는 웹팩 로더
+- `style-loader`, `css-loader` : css파일을 병합하기 위한 웹팩 로더
 
 
-2. `tsconfig.json`설정. 
+## 설정 파일
+1. `tsconfig.json`설정. 
     - `npx tsc --init` 또는 파일을 복사해옴
+2. `webpack.config.js` 복사 후 설정.
 
-3. `webpack.config.js` 복사 후 설정.
 
 
 
--- dist/scroll-top.class.js
+++ dist/scroll-top.class.js
@@ -1,30 +1,28 @@
 define("scroll-top", ["require", "exports"], function (require, exports) {
     "use strict";
-    exports.__esModule = true;
+    Object.defineProperty(exports, "__esModule", { value: true });
     /*!
-    * exizt/scroll-to-top v3.0.5
+    * exizt/scroll-to-top v4.0.6
     *
     *  License : MIT
     *      Git : https://github.com/exizt/scroll-to-top
     *   Author : EXIzT
     */
-    var shScrollToTop = /** @class */ (function () {
-        function shScrollToTop() {
-            var _this = this;
+    class shScrollToTop {
+        constructor() {
             this.scrollBase = 100;
             this.ticking = false;
             this.displaying = false;
             this.arrowId = "shScrollTop";
             this.isDebug = true;
-            document.addEventListener("DOMContentLoaded", function () {
-                _this.load();
+            document.addEventListener("DOMContentLoaded", () => {
+                this.load();
             });
         }
         /**
          * 화살표 생성 및 이벤트 바인딩 등
          */
-        shScrollToTop.prototype.load = function () {
-            var _this = this;
+        load() {
             var _a;
             // requestAnimationFrame은 ie10 이상
             // https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
@@ -33,27 +31,27 @@ define("scroll-top", ["require", "exports"], function (require, exports) {
             // 화살표를 draw
             this.insertArrowHTML();
             // scroll 리스너를 등록
-            window.addEventListener('scroll', function (e) {
-                if (!_this.ticking) {
+            window.addEventListener('scroll', (e) => {
+                if (!this.ticking) {
                     // this.debugLog('scroll event')
-                    window.requestAnimationFrame(function () {
-                        _this.scrollEvent(_this.getScrollY());
-                        _this.ticking = false;
+                    window.requestAnimationFrame(() => {
+                        this.scrollEvent(this.getScrollY());
+                        this.ticking = false;
                     });
-                    _this.ticking = true;
+                    this.ticking = true;
                 }
             });
             // 클릭 이벤트 바인딩
-            (_a = document.getElementById(this.arrowId)) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
-                _this.scrollToTop();
+            (_a = document.getElementById(this.arrowId)) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+                this.scrollToTop();
             });
-        };
+        }
         /**
          * 상단으로 스크롤.
          */
-        shScrollToTop.prototype.scrollToTop = function () {
+        scrollToTop() {
             // https://stackoverflow.com/questions/52276194/window-scrollto-with-options-not-working-on-microsoft-edge
-            var supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
+            const supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
             if (supportsNativeSmoothScroll) {
                 ///// 모던 브라우저의 경우
                 // https://developer.mozilla.org/en-US/docs/Web/API/ScrollToOptions/behavior
@@ -66,74 +64,76 @@ define("scroll-top", ["require", "exports"], function (require, exports) {
                 // https://stackoverflow.com/questions/15935318/smooth-scroll-to-top/48942924
                 // https://stackoverflow.com/questions/42261524/how-to-window-scrollto-with-a-smooth-effect
                 this.debugLog('scrollToTop(). smoothScroll().');
-                var smoothScroll_1 = function (h) {
-                    var i = h || 0;
+                const smoothScroll = (h) => {
+                    const i = h || 0;
                     if (i > 10) {
-                        setTimeout(function () {
+                        setTimeout(() => {
                             window.scrollTo(0, i);
-                            smoothScroll_1(i * 0.9);
+                            smoothScroll(i * 0.9);
                         }, 10);
                     }
                     else {
                         window.scrollTo(0, 0);
                     }
                 };
-                smoothScroll_1(this.getScrollY());
+                smoothScroll(this.getScrollY());
             }
-        };
+        }
         /**
          * 화살표를 표시하는 html 요소를 추가
          */
-        shScrollToTop.prototype.insertArrowHTML = function () {
+        insertArrowHTML() {
             var _a;
-            var html = "<div class=\"st-scrolltop-wrap\"><div id=\"".concat(this.arrowId, "\" class=\"scrolltop\" style=\"display:none\">\n\t\t<div class=\"arrow\"></div>\n\t  </div></div>");
+            const html = `<div class="st-scrolltop-wrap"><div id="${this.arrowId}" class="scrolltop" style="display:none">
+		<div class="arrow"></div>
+	  </div></div>`;
             (_a = document.querySelector("body")) === null || _a === void 0 ? void 0 : _a.insertAdjacentHTML('beforeend', html);
-        };
+        }
         /**
          * scroll event 에서 fadeIn, fadeOut 설정
          * @param scrollY 스크롤 Y 좌표
          */
-        shScrollToTop.prototype.scrollEvent = function (scrollY) {
+        scrollEvent(scrollY) {
             if (typeof scrollY === 'undefined') {
-                this.debugLog("scroll Y : ".concat(scrollY));
+                this.debugLog(`scroll Y : ${scrollY}`);
             }
             if (scrollY > this.scrollBase) {
                 if (this.displaying === false) {
-                    var el = document.getElementById(this.arrowId);
+                    const el = document.getElementById(this.arrowId);
                     this.fadeIn(el, 0.3);
                     this.displaying = true;
                 }
             }
             else {
                 if (this.displaying === true) {
-                    var el = document.getElementById(this.arrowId);
+                    const el = document.getElementById(this.arrowId);
                     this.fadeOut(el);
                     this.displaying = false;
                 }
             }
-        };
+        }
         /**
          * y 좌표를 구함
          * (ie 9 이상. ie에서는 window.pageYOffset을 이용함. 모던브라우저에서는 window.scrollY 또는 window.pageYOffset 이용)
          * https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
          * @returns
          */
-        shScrollToTop.prototype.getScrollY = function () {
+        getScrollY() {
             return window.pageYOffset;
             // return window.scrollY
-        };
+        }
         /**
          * y 좌표를 구하는 메소드
          * https://developer.mozilla.org/ko/docs/Web/API/Window/scrollY
          * @returns number
          * @deprecated
          */
-        shScrollToTop.prototype.getScrollY_Legacy = function () {
-            var isSupportPageOffset = window.pageXOffset !== undefined;
-            var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-            var y = isSupportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+        getScrollY_Legacy() {
+            const isSupportPageOffset = window.pageXOffset !== undefined;
+            const isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+            const y = isSupportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
             return y;
-        };
+        }
         /**
          * fadeIn(천천히 나타남)을 구현하는 메소드
          * https://www.ilearnjavascript.com/plainjs-fadein-fadeout/
@@ -142,77 +142,71 @@ define("scroll-top", ["require", "exports"], function (require, exports) {
          * @param smooth
          * @param displayStyle
          */
-        shScrollToTop.prototype.fadeIn = function (el, _opacity, smooth, displayStyle) {
-            if (_opacity === void 0) { _opacity = 1; }
-            if (smooth === void 0) { smooth = true; }
-            if (displayStyle === void 0) { displayStyle = 'block'; }
+        fadeIn(el, _opacity = 1, smooth = true, displayStyle = 'block') {
             if (!!!el)
                 return;
             el.style.opacity = 0;
             el.style.display = displayStyle;
             if (smooth) {
-                var opacity_1 = 0;
-                var request_1;
-                var animation_1 = function () {
-                    opacity_1 += 0.02;
-                    if (opacity_1 >= _opacity) {
-                        opacity_1 = _opacity;
-                        cancelAnimationFrame(request_1);
+                let opacity = 0;
+                let request;
+                const animation = () => {
+                    opacity += 0.02;
+                    if (opacity >= _opacity) {
+                        opacity = _opacity;
+                        cancelAnimationFrame(request);
                     }
-                    el.style.opacity = opacity_1;
+                    el.style.opacity = opacity;
                 };
-                var rAf_1 = function () {
-                    request_1 = requestAnimationFrame(rAf_1);
-                    animation_1();
+                const rAf = () => {
+                    request = requestAnimationFrame(rAf);
+                    animation();
                 };
-                rAf_1();
+                rAf();
             }
             else {
                 el.style.opacity = 1;
             }
-        };
+        }
         /**
          * fadeOut(천천히 없어짐)을 구현하는 메소드
          * @param el
          * @param smooth
          * @param displayStyle
          */
-        shScrollToTop.prototype.fadeOut = function (el, smooth, displayStyle) {
-            if (smooth === void 0) { smooth = true; }
-            if (displayStyle === void 0) { displayStyle = 'none'; }
+        fadeOut(el, smooth = true, displayStyle = 'none') {
             if (!!!el)
                 return;
             if (smooth) {
-                var opacity_2 = el.style.opacity;
-                var request_2;
-                var animation_2 = function () {
-                    opacity_2 -= 0.04;
-                    if (opacity_2 <= 0) {
-                        opacity_2 = 0;
+                let opacity = el.style.opacity;
+                let request;
+                const animation = () => {
+                    opacity -= 0.04;
+                    if (opacity <= 0) {
+                        opacity = 0;
                         el.style.display = displayStyle;
-                        cancelAnimationFrame(request_2);
+                        cancelAnimationFrame(request);
                     }
-                    el.style.opacity = opacity_2;
+                    el.style.opacity = opacity;
                 };
-                var rAf_2 = function () {
-                    request_2 = requestAnimationFrame(rAf_2);
-                    animation_2();
+                const rAf = () => {
+                    request = requestAnimationFrame(rAf);
+                    animation();
                 };
-                rAf_2();
+                rAf();
             }
             else {
                 el.style.opacity = 0;
             }
-        };
+        }
         /**
          * 디버깅 로그
          * @param msg 디버깅 로그
          */
-        shScrollToTop.prototype.debugLog = function (msg) {
+        debugLog(msg) {
             if (this.isDebug)
                 console.log('[ScrollTop] ', msg);
-        };
-        return shScrollToTop;
-    }());
-    exports["default"] = shScrollToTop;
+        }
+    }
+    exports.default = shScrollToTop;
 });
