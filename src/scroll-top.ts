2d1effda8a3b33e577f2dc66d82fe5cb3c511cf3
exizt <e2xist@gmail.com>
2022-08-25 07:58:17 +0900

    코드 전체적으로 수정.

--- src/scroll-top.ts
+++ src/scroll-top.ts
@@ -6,284 +6,230 @@
 *  Author : exizt
 */
 export class ScrollTop {
-	// 화살표가 나타나는 기준선
-	private scrollBase = 100
-	private elementId = "shScrollTop"
-	private displaying = false
-	private isDebug = false
-	// (중복 방지 기능) 기능이 로드되었는지 여부 status. 
-	private isLoaded = false
-	// (중복 방지 기능) DOMContentLoaded 이벤트 바인딩 여부 status.
-	private isDomLoadedEventBinded = false
-	// (중복 방지 기능) animationFrame 중복 방지 status.
-	private scheduledAnimationFrame = false
-	// 이벤트 핸들러. add, remove를 위해 포인터를 지니기 위함.
-	private scrollEventHandler!: EventListener
+    // 화살표가 나타나는 기준선
+    private scrollBase = 100
+    private elementId = "shScrollTop"
+    private displaying = false
+    private isDebug = false
+    // (중복 방지 기능) 기능이 로드되었는지 여부 status. 
+    private isLoaded = false
+    // (중복 방지 기능) DOMContentLoaded 이벤트 바인딩 여부 status.
+    private isDomLoadedEventBinded = false
+    // (중복 방지 기능) animationFrame 중복 방지 status.
+    private isRunningScrollRaf = false
+    // 이벤트 핸들러. add, remove를 위해 포인터를 지니기 위함.
+    private scrollEventHandler!: EventListener
 
-	constructor(){
-	}
+    constructor(){
+    }
 
-	/**
-	 * 이벤트 바인딩
-	 */
-	load(options?:IOptions): void {
-		// 옵션이 있을 경우 옵션값 지정
-		this.setOptions(options)
+    /**
+     * 이벤트 바인딩
+     */
+    load(options?:IOptions): void {
+        // 옵션이 있을 경우 옵션값 지정
+        this.setOptions(options)
 
-		// 중복 로드 방지
-		if(this.isLoaded) return
+        // 중복 로드 방지
+        if(this.isLoaded) return
 
-		if(!this.isDomLoadedEventBinded){
-			document.addEventListener("DOMContentLoaded",()=>{
-				// requestAnimationFrame은 ie10 이상
-				// https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
-				if(typeof requestAnimationFrame !== 'function') return
-				
-				// 화살표를 draw
-				this.insertSymbolHTML()
-	
-				// scroll 리스너를 등록
-				this.scrollEventHandler = (e) => this.scrollAnimate()
-				window.addEventListener('scroll', this.scrollEventHandler)
-			
-				// 클릭 이벤트 바인딩
-				document.getElementById(this.elementId)?.addEventListener('click',()=>{
-					this.scrollToTop()
-				})
-			})
-			this.isDomLoadedEventBinded = true
-			this.isLoaded = true
-		} else {
-			// scroll 리스너를 재등록
-			window.addEventListener('scroll', this.scrollEventHandler)
-			this.isLoaded = true
-		}
-	}
+        // requestAnimationFrame은 ie10 이상
+        // https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
+        if(typeof requestAnimationFrame !== 'function') return
+        if(typeof window.scrollY === 'undefined') return
 
-	/**
-	 * 스크롤 이벤트 해제 등. 기능 정지.
-	 */
-	unload(): void {
-		// 스크롤 이벤트에서 해제
-		if(!this.isLoaded) return
-		window.removeEventListener('scroll', this.scrollEventHandler)
-		this.isLoaded = false
-
-		// 화살표시를 hidden 처리
-		const el = document.getElementById(this.elementId)
-		this.fadeOut(el, false)
-		this.displaying = false
-	}
-
-	/**
-	 * 옵션값 지정
-	 * @param options 옵션값 JSON
-	 */
-	setOptions(options?:IOptions): void{
-		if(!options) return
-		if(options.base){
-			this.scrollBase = options.base
-		}
-		if(options.isDebug){
-			this.isDebug = options.isDebug
-		}
-	}
-
-	/**
-	 * 스크롤 이벤트 바인딩
-	 */
-	scrollAnimate(){
-		this.debugLog("scrolling")
-		// 단순히 스크롤 이벤트 바인딩을 할 경우에는, 상황에 따라 렌더링 등에 무리가 갈 수 있다.
-		// 렌더링에 무리가 가지않게 해주는 requestAnimationFrame을 이용하여 
-		// 호출되는 횟수를 적절히 줄여준다.
-		// https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
-		// https://stackoverflow.com/a/44779316
-		if (this.scheduledAnimationFrame) {
-			return
-		}
-		this.debugLog('scrollAnimate()', 'call requestAnimationFrame(fadeInOutByScrollY)')
-		// window.requestAnimationFrame
-		requestAnimationFrame(()=>{
-			this.fadeInOutByScrollY(this.getScrollY())
-			this.scheduledAnimationFrame = false
-		})
-		this.scheduledAnimationFrame = true
-	}
-
-	/**
-	 * 화살표를 표시하는 html 요소를 추가
-	 */
-	insertSymbolHTML(){
-		const html = `<div class="st-scrolltop-wrap"><div id="${this.elementId}" class="scrolltop" style="display:none">
-		<div class="arrow"></div>
-	  </div></div>`
-		document.querySelector("body")?.insertAdjacentHTML('beforeend', html)
-	}
-
-	/**
-	 * scroll event 에서 fadeIn, fadeOut 설정
-	 * @param scrollY 스크롤 Y 좌표
-	 */
-	fadeInOutByScrollY(scrollY:number) {
-		if(typeof scrollY === 'undefined'){
-			if(this.isDebug){
-				this.debugLog('scroll Y is undefined')
-			}
-			return
-		}
-		if(this.isDebug){
-			this.debugLog('scroll Y : ', scrollY)
-		}
-		if(scrollY > this.scrollBase){
-			if(this.displaying === false){
-				const el = document.getElementById(this.elementId)
-				this.fadeIn(el, 0.3)
-				this.displaying = true
-			}
-		} else {
-			if(this.displaying === true){
-				const el = document.getElementById(this.elementId)
-				this.fadeOut(el)
-				this.displaying = false
-			}
-		}
-	}
-	
-	/**
-	 * y 좌표를 구함
-	 * (ie 9 이상. ie에서는 window.pageYOffset을 이용함. 모던브라우저에서는 window.scrollY 또는 window.pageYOffset 이용)
-	 * https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
-	 * @returns number
-	 */
-	getScrollY():number{
-		return window.pageYOffset
-		// return window.scrollY
-	}
-
-	/**
-	 * fadeIn(천천히 나타남)을 구현하는 메소드
-	 * https://www.ilearnjavascript.com/plainjs-fadein-fadeout/
-	 * @param el 
-	 * @param _opacity 
-	 * @param smooth 
-	 * @param displayStyle 
-	 * @deprecated
-	 */
-	fadeIn(el:any, _opacity = 1, smooth = true, displayStyle = 'block') {
-		if(!!!el) return
-		el.style.opacity = 0;
-		el.style.display = displayStyle;
-		if (smooth) {
-			let opacity = 0;
-			let request:number
-
-			const animation = () => {
-				opacity += 0.02
-				if (opacity >= _opacity) {
-					opacity = _opacity
-					cancelAnimationFrame(request)
-				}
-				el.style.opacity = opacity
-			}
-
-			const rAf = () => {
-				request = requestAnimationFrame(rAf)
-				animation()
-			}
-			rAf()
-
-		} else {
-			el.style.opacity = 1;
-		}
-	}
-
-	/**
-	 * fadeOut(천천히 없어짐)을 구현하는 메소드
-	 * @param el 
-	 * @param smooth 
-	 * @param displayStyle 
-	 * @deprecated
-	 */
-	fadeOut(el:any, smooth = true, displayStyle = 'none'){
-		if(!!!el) return
-		if (smooth) {
-			let opacity = el.style.opacity;
-			let request:number
-
-			const animation = () => {
-				opacity -= 0.04;
-				if (opacity <= 0) {
-					opacity = 0;
-					el.style.display = displayStyle;
-					cancelAnimationFrame(request);
-				}
-				el.style.opacity = opacity
-			}
-
-			const rAf = () => {
-				request = requestAnimationFrame(rAf)
-				animation()
-			}
-			rAf()
-
-		} else {
-			el.style.opacity = 0;
-		}
-	}
-
-	/**
-	 * 상단으로 스크롤.
-	 */
-	 scrollToTop(){
-		// https://stackoverflow.com/questions/52276194/window-scrollto-with-options-not-working-on-microsoft-edge
-		const supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
-		if (supportsNativeSmoothScroll) {
-			///// 모던 브라우저의 경우
-			// https://developer.mozilla.org/en-US/docs/Web/API/ScrollToOptions/behavior
-			// behavior는 explorer 에서는 안 됨(ie11 에서도 안 됨)
-			this.debugLog('scrollToTop(). behavior smooth.')
-			window.scroll({top:0, behavior: "smooth"})
-		} else {
-			///// 예전 브라우저의 경우 (예: ie11)
-			// https://stackoverflow.com/questions/15935318/smooth-scroll-to-top/48942924
-			// https://stackoverflow.com/questions/42261524/how-to-window-scrollto-with-a-smooth-effect
-			this.debugLog('scrollToTop(). smoothScroll().')
-
-			const smoothScroll = (h:number) => {
-				const i = h || 0;
-				if (i > 10) {
-				  setTimeout(() => {
-					window.scrollTo(0, i);
-					smoothScroll(i * 0.9);
-				  }, 10);
-				} else {
-					window.scrollTo(0, 0);
-				}
-			}
-			smoothScroll(this.getScrollY())
-		}
-	}
-
-	/**
-	 * 디버깅 로그
-	 * @param _args 디버깅 로그
-	 */
-	debugLog(..._args:any){
-		if (this.isDebug) {
-            const tag = '[ScrollTop]'
-			const args = _args.map((x: any) => {
-				if(typeof x === 'object'){
-					return JSON.parse(JSON.stringify(x))
-				} else {
-					return x
-				}
-			})
-			console.log(tag, ...args)
+        // domLoaded 이벤트 바인딩
+        if(!this.isDomLoadedEventBinded){
+            document.addEventListener("DOMContentLoaded",()=>{
+                
+                // 화살표를 draw
+                this.insertSymbolHTML()
+    
+                // scroll 리스너를 등록
+                this.scrollEventHandler = () => this.scrollAnimate()
+                window.addEventListener('scroll', this.scrollEventHandler)
+            
+                // 클릭 이벤트 바인딩
+                document.getElementById(this.elementId)?.addEventListener('click',(e)=>{
+                    this.scrollToTop()
+                })
+            })
+            this.isDomLoadedEventBinded = true
+            this.isLoaded = true
+        } else {
+            // scroll 리스너를 재등록
+            window.addEventListener('scroll', this.scrollEventHandler)
+            const el = document.getElementById(this.elementId)
+            if(!!el){
+                el.style.display = "block"
+            }
+            this.isLoaded = true
         }
-	}
+    }
+
+    /**
+     * 스크롤 이벤트 해제 등. 기능 정지.
+     */
+    unload(): void {
+        if(!this.isLoaded) return
+        
+        // 스크롤 이벤트에서 해제
+        window.removeEventListener('scroll', this.scrollEventHandler)
+        this.isLoaded = false
+
+        // 화살표시를 hidden 처리
+        const el = document.getElementById(this.elementId)
+        if(!!el){
+            el.style.opacity = "0"
+            el.style.display = "none"
+        }
+        this.displaying = false
+    }
+
+    /**
+     * 옵션값 지정
+     * @param options 옵션값 JSON
+     */
+    private setOptions(options?:IOptions): void{
+        if(!options) return
+        if(options.base){
+            this.scrollBase = options.base
+        }
+        if(options.isDebug){
+            this.isDebug = options.isDebug
+        }
+    }
+
+    /**
+     * 스크롤 이벤트 바인딩
+     */
+    private scrollAnimate(){
+        // 사용자가 스크롤 이벤트를 하는 데에는 문제가 없으나, scroll() 등으로 이동 중에 
+        // 무리가 발생할 수 있으므로, 이를 예방하기 위해 requestAnimationFrame을 사용한다.
+        // requestAnimiationFrame은 1/60s 정도로 동작된다는 듯.
+        // 최근 브라우저에서는 사용하지 않아도 큰 문제는 없는 듯..
+        // https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+        // https://stackoverflow.com/a/44779316
+        if (this.isRunningScrollRaf) {
+            return
+        }
+        const run = ()=>{
+            this.fadeInOutByScrollY(this.getScrollY())
+            this.isRunningScrollRaf = false
+        }
+        this.isRunningScrollRaf = true
+        // window.requestAnimationFrame
+        requestAnimationFrame(run)
+    }
+
+    /**
+     * 화살표를 표시하는 html 요소를 추가
+     */
+    private insertSymbolHTML(){
+        const html = `<div class="st-scrolltop-wrap"><div id="${this.elementId}" class="st-scrolltop">
+        <div class="st-arrow"></div>
+      </div></div>`
+        document.querySelector("body")?.insertAdjacentHTML('beforeend', html)
+    }
+
+    /**
+     * scroll event 에서 fadeIn, fadeOut 설정
+     * @param scrollY 스크롤 Y 좌표
+     */
+    private fadeInOutByScrollY(scrollY:number) {
+        if(typeof scrollY === 'undefined'){
+            if(this.isDebug){
+                this.debugLog('scroll Y is undefined')
+            }
+            return
+        }
+        if(this.isDebug){
+            this.debugLog('scroll Y : ', scrollY)
+        }
+        if(scrollY > this.scrollBase){
+            if(this.displaying === false){
+                const el = document.getElementById(this.elementId)
+                if(!!el){
+                    el.style.opacity = "0.3"
+                }
+                this.displaying = true
+            }
+        } else {
+            if(this.displaying === true){
+                const el = document.getElementById(this.elementId)
+                if(!!el){
+                    el.style.opacity = "0"
+                }
+                this.displaying = false
+            }
+        }
+    }
+    
+    /**
+     * y 좌표를 구함
+     * (ie 9 이상. ie에서는 window.pageYOffset을 이용함. 모던브라우저에서는 window.scrollY 또는 window.pageYOffset 이용)
+     * 모던 브라우저 : window.scrollY
+     * ie 11 : window.pageYOffset
+     * ie 10 이하 : 복잡. document.documentElement.scrollTop, document.body.scrollTop 등
+     * https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+     * @returns number
+     */
+    private getScrollY():number{
+        // return window.pageYOffset
+        return window.scrollY
+    }
+
+    /**
+     * 상단으로 스크롤.
+     */
+    private scrollToTop(){
+        // https://stackoverflow.com/questions/52276194/window-scrollto-with-options-not-working-on-microsoft-edge
+        const supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
+        if (supportsNativeSmoothScroll) {
+            ///// 모던 브라우저의 경우
+            // https://developer.mozilla.org/en-US/docs/Web/API/Window/scroll
+            // behavior는 explorer 에서는 안 됨(ie11 에서도 안 됨)
+            this.debugLog('scrollToTop(). behavior smooth.')
+            window.scroll({top:0, behavior: "smooth"})
+        } else {
+            ///// 예전 브라우저의 경우 (예: ie11)
+            // https://stackoverflow.com/questions/15935318/smooth-scroll-to-top/48942924
+            // https://stackoverflow.com/questions/42261524/how-to-window-scrollto-with-a-smooth-effect
+            this.debugLog('scrollToTop(). smoothScroll().')
+
+            const smoothScroll = (h:number) => {
+                const i = h || 0;
+                if (i > 10) {
+                  setTimeout(() => {
+                    window.scrollTo(0, i);
+                    smoothScroll(i * 0.9);
+                  }, 10);
+                } else {
+                    window.scrollTo(0, 0);
+                }
+            }
+            smoothScroll(this.getScrollY())
+        }
+    }
+
+    /**
+     * 디버깅 로그
+     * @param _args 디버깅 로그
+     */
+    private debugLog(..._args:any){
+        if (!this.isDebug) return
+        const tag = '[ScrollTop]'
+        const args = _args.map((x: any) => {
+            if(typeof x === 'object'){
+                return JSON.parse(JSON.stringify(x))
+            } else {
+                return x
+            }
+        })
+        console.log(tag, ...args)
+    }
 }
 interface IOptions {
-	base: number;
+    base: number;
     isDebug: boolean;
 }
